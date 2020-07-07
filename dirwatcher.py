048efc6fce0fb83057c5c8549ad9356e3040c2bc
Nikal Morgan <nikalmorgan11@gmail.com>
2020-07-06 19:50:10 -0500

    Program description, imported libraries, global variables, and exit message

--- dirwatcher.py
+++ dirwatcher.py
@@ -1,9 +1,22 @@
-__author__ = "Nikal Morgan"
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Thi program monitors a given directory for text files that are created within
+the monitored directory. It will continually search within all files in the
+directory for a given "magic string" (implemented with a timed polling loop).
+If the magic string is found in a file, the program will log a message
+indicating which file, and the line number within the file where the magic
+text was found; it will not be logged again unless it appears in the file as
+another subsequent line entry later on.
+"""
+
 
 import logging
 import sys
 import signal
 import time
+
+__author__ = "Nikal Morgan"
 
 logger = logging.getLogger(__name__)
 
@@ -13,8 +26,10 @@ exit_flag = False
 
 def signal_handler(sig_num, frame):
     """
-    This is a handler for SIGTERM and SIGINT. Other signals can be mapped here as well (SIGHUP?)
-    Basically, it just sets a global flag, and main() will exit its loop if the signal is trapped.
+    This is a handler for SIGTERM and SIGINT.
+    Other signals can be mapped here as well (SIGHUP?)
+    Basically, it just sets a global flag, and main() will exit its loop
+    if the signal is trapped.
     :param sig_num: The integer signal number that was trapped from the OS.
     :param frame: Not used
     :return None
@@ -31,22 +46,21 @@ def main():
     signal.signal(signal.SIGTERM, signal_handler)
     # Now my signal_handler will get called if OS sends
     # either of these to my process.
+    start_time = time.time()
 
     while not exit_flag:
         try:
             # call my directory watching function
+            # put a sleep inside my while loop
+            # so I don't peg the cpu usage at 100%
+            time.sleep(polling_interval)
             pass
         except Exception as e:
-            # This is an UNHANDLED exception
-            # Log an ERROR level message here
-            pass
-
-        # put a sleep inside my while loop so I don't peg the cpu usage at 100%
-        time.sleep(polling_interval)
-
-    # final exit point happens here
-    # Log a message that we are shutting down
-    # Include the overall uptime since program start
+            logger.exception(e)
+            time.sleep(10)
+    
+    logger.info("\nExiting.\n"
+                "Process ran for {} seconds".format(time.time() - start_time))
 
     if __name__ == "__main__":
         """Runs the main loop until an interrupt like control+c are input."""
