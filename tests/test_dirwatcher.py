47f460a64dac3cd62d1ff8ffac220cd652f433f2
Nikal Morgan <nikalmorgan11@gmail.com>
2020-07-06 20:43:07 -0500

    Added methods to TestDirwatcher class

--- tests/test_dirwatcher.py
+++ tests/test_dirwatcher.py
@@ -1,11 +1,50 @@
 #!/usr/bin/env python
 
 import unittest
+import sys
+import importlib
+import inspect
+import argparse
+
 
 __author__ = 'Nikal Morgan'
 
+# devs: change this to 'soln.dirwatcher' to run this suite against the solution
+PKG_NAME = 'dirwatcher'
+
 
 class TestDirwatcher(unittest.TestCase):
+    """Main test fixture for 'echo' module"""
+    @classmethod
+    def setUpClass(cls):
+        """Performs module import and suite setup at test-runtime"""
+        # check for python3
+        cls.assertGreaterEqual(cls, sys.version_info[0], 3)
+        # This will import the module to be tested
+        cls.module = importlib.import_module(PKG_NAME)
+        # Make a dictionary of each function in the student's test module
+        cls.funcs = {
+            k: v for k, v in inspect.getmembers(
+                cls.module, inspect.isfunction
+            )
+        }
+        # check the module for required functions
+        assert "main" in cls.funcs, "Missing required function main()"
+        assert "create_parser" in cls.funcs, "Missing required function "
+        "create_parser()"
+
+    def setUp(self):
+        """Called by parent class ONCE before all tests are run"""
+        # your code here - use this space to create any instance variables
+        # that will be visible to your other test methods
+        self.parser = self.module.create_parser()
+
+    def test_parser(self):
+        """Check if create_parser() returns a parser object"""
+        result = self.module.create_parser()
+        self.assertIsInstance(
+            result, argparse.ArgumentParser,
+            "create_parser() function is not returning a parser object")
 
     def test_prog(self):
         pass
